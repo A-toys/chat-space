ea85d1a02a137e781b697e30b575fc4745090719
A-toys <nao_lespaul_jr@yahoo.co.jp>
2019-11-18 14:29:02 +0900

    add ajax to message posting function

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,64 @@
+$(function() {
+
+  function buildMessage(message) {
+    var html = message.image !== null ? (
+               `<div class="message">
+                  <div class="message__info">
+                    <div class="message__info__writer">
+                    ${message.user}
+                    </div>
+                    <div class="message__info__post-day">
+                      ${message.created_at}
+                    </div>
+                  </div>
+                  <div class="lower-message">
+                    <p class="message__text">
+                      ${message.content}
+                    </p>
+                    <img src="${message.image}", class="lower-message__image" > 
+                  </div>
+                </div>` 
+    ) : (
+               `<div class="message">
+                  <div class="message__info">
+                    <div class="message__info__writer">
+                      ${message.user}
+                    </div>
+                    <div class="message__info__post-day">
+                      ${message.created_at}
+                    </div>
+                  </div>
+                  <div class="lower-message">
+                    <p class="message__text">
+                      ${message.content}
+                    </p>
+                  </div>
+                </div>`
+    );
+    return html;
+  }
+
+  $('.new_message').on('submit', function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action')
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(message){
+      var html = buildMessage(message);
+      $('.main-content').append(html);
+      $('.main-content').animate({scrollTop : $('.main-content')[0].scrollHeight});
+      $('.new_message')[0].reset();
+      $('.form__submit').prop('disabled', false);
+    })
+    .fail(function() {
+      alert("メッセージ送信に失敗しました");
+    })
+  })
+})
